72005974a80bf0107386fb90ae462efff2f38e2d
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-03-07 17:56:20 -0300

    line endings

--- test/test-flux.js
+++ test/test-flux.js
@@ -14,32 +14,32 @@ var index = require("../index.js");
 // - tests
 
 describe("flux",function() {
-  
+
   it("constructor",function() {
     var flux = new index.Flux();
     assert.ok(flux.dispatcher instanceof index.Dispatcher);
   });
-  
+
   it("constructor with dispatcher",function() {
     var dispatcher = new index.Dispatcher();
     var flux = new index.Flux(dispatcher);
     assert.strictEqual(flux.dispatcher,dispatcher);
   });
 
-  it("createStore",function() {    
+  it("createStore",function() {
     var flux = new index.Flux();
     var store = flux.createStore();
     assert.ok(store instanceof index.Store);
     assert.strictEqual(store.dispatcher,flux.dispatcher);
   });
 
-  it("createAction",function() {    
+  it("createAction",function() {
     var flux = new index.Flux();
     var action = flux.createAction();
     assert.ok(action instanceof index.Action);
     assert.strictEqual(action.dispatcher,flux.dispatcher);
   });
-  
+
 });
 
 // - -------------------------------------------------------------------- - //
